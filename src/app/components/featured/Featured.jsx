092215f228e2aa06a23de16156be2fd4b1c44418
Iqbal Alhuda <iqbal.alhuda@bisnis.com>
2023-12-21 14:15:36 +0700

    New Feature: pembuatan component featured

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import Image from "next/image";
 
 function Featured() {
-  return <div>Featured</div>;
+  return (
+    <div className="mt-8">
+      <h1 className="text-7xl leading-snug">
+        <b>Hey, Explore Kuliner here!</b> Discover culinary delights and ideas
+        around you.
+      </h1>
+      <div className="mt-16 flex text-center gap-12">
+        <div className="flex-1 h-[500px] relative">
+          <Image
+            src="/food/featured.jpg"
+            alt="featured-image"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="flex flex-1 flex-col gap-5 text-start justify-center">
+          <h1 className="text-4xl font-bold leading-tight">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className="text-xl font-light leading-tight text-[#626262]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            impedit accusantium, obcaecati natus modi fugiat libero velit atque
+            explicabo earum? Vel commodi odit ad rerum inventore earum labore
+            culpa quibusdam.
+          </p>
+          <button className="px-5 py-4 border-none rounded-[5px] w-max bg-gray-200">
+            Read More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Featured;
