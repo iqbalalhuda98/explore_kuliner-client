2908082c950c5c01ce5e5b54496e0a0f080b39fd
Iqbal Alhuda <iqbal.alhuda@bisnis.com>
2023-12-21 16:29:53 +0700

    New Feature: pembuatan component category, mobile responsive feature dan category

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function CategoryList() {
-  return <div>CategoryList</div>;
+  return (
+    <div>
+      <h1 className="my-12 text-3xl font-extrabold">Popular Categories</h1>
+      <div className="flex flex-wrap justify-between gap-5">
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#57c4ff31] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/coding.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category1
+        </Link>
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#da85c731] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/culture.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category2
+        </Link>
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#7fb88133] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/fashion.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category3
+        </Link>
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#ff795736] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/food.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category4
+        </Link>
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#ffb04f45] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/style.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category5
+        </Link>
+        <Link
+          href={`/blog`}
+          className="flex items-center gap-[10px] w-[100%] sm:w-[45%] md:w-[25%] lg:w-[15%] h-20 justify-center bg-[#5e4fff31] rounded-[10px] font-medium"
+        >
+          <div className="relative w-8 h-8">
+            <Image
+              src="/iconPopular/travel.png"
+              alt=""
+              fill
+              className="rounded-full"
+            />
+          </div>
+          category6
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default CategoryList;
--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 function Featured() {
   return (
     <div className="mt-8">
-      <h1 className="text-7xl leading-snug">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-snug">
         <b>Hey, Explore Kuliner here!</b> Discover culinary delights and ideas
         around you.
       </h1>
       <div className="mt-16 flex text-center gap-12">
-        <div className="flex-1 h-[500px] relative">
+        <div className="flex-1 h-[500px] relative hidden lg:flex">
           <Image
             src="/food/featured.jpg"
             alt="featured-image"
@@ -17,10 +17,10 @@ function Featured() {
           />
         </div>
         <div className="flex flex-1 flex-col gap-5 text-start justify-center">
-          <h1 className="text-4xl font-bold leading-tight">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold leading-tight">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </h1>
-          <p className="text-xl font-light leading-tight text-[#626262]">
+          <p className="text-base md:text-lg lg:text-xl font-light leading-tight text-[#626262]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
             impedit accusantium, obcaecati natus modi fugiat libero velit atque
             explicabo earum? Vel commodi odit ad rerum inventore earum labore
